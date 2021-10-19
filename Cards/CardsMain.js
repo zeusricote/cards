779dbdfba2c32e78ed940e5eba907ab0744f36fc
zeusricote <92752554+zeusricote@users.noreply.github.com>
2021-10-19 08:55:22 +0200

    Create CardsMain.js

---
+++ Cards/CardsMain.js
@@ -0,0 +1,20 @@
+//Black cards (Questions)
+var cards_blackCards = [
+   "Test Question One ____?",
+   "Test Question Two ____?",
+   "Test Question Three ____?"
+ ]
+
+//White cards (Answers)
+var cards_whiteCards = [
+   "Test Answer One?",
+   "Test Answer Two?",
+   "Test Answer Three?",
+   "Test Answer Four?",
+   "Test Answer Five?",
+   "Test Answer Six?",
+   "Test Answer Seven?",
+   "Test Answer Eight?",
+   "Test Answer Nine?",
+   "Test Answer Ten?"
+ ]
